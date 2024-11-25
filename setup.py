edc220ab4195b8e2aee2e4e51bc5cd58c63014fa
anish <anish5256@gmail.com>
2024-11-25 13:41:57 +0530

    Bug fix search bar is not working

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-lazy-admin-pagination',
-    version='1.0.1',
+    version='1.0.2',
     description='A Django package for lazy-loading pagination in the admin interface.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
